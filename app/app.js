13ee07459e6192f52d37b145ebd6b19e5001402d
Fennerwe <fennerwe13@gmail.com>
2015-10-05 11:28:35 -0400

    Home page set up and working

---
+++ app/app.js
@@ -0,0 +1,19 @@
+angular.module('app', ['ui.router']);
+					   
+angular.module('app').config(function($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise('/home');
+	
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: 'app/home.tpl.html'
+		})
+		.state('users', {
+			url: '/users',
+			templateUrl: 'app/users/users.tpl.html'
+		})
+		.state('projects', {
+			url: '/projects',
+			templateUrl: 'app/projects/projects.tpl.html'
+		});
+});
