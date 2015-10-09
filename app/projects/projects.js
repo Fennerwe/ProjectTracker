d99f04aa8217431f197a4b31c695771a142ffdd8
Fennerwe <fennerwe13@gmail.com>
2015-10-09 11:43:43 -0400

    Added projects main page and module

---
+++ app/projects/projects.js
@@ -0,0 +1,22 @@
+angular.module('projects', ['ui-router']);
+
+angular.module('projects').config(function($stateProvider, $urlRouterProvider){
+		
+		$stateProvider.state('project', {
+			url: 'project/:projectID',
+			templateUrl: 'projectDash/project-dash.tpl.html',
+			controller: 'projectDashCtrl'
+		});
+});
+
+angular.module('projects').controller('projectsCtrl', ['$scope', '$http', function($scope, $http){
+	
+}]);
+
+angular.module('projects').directive('projectCard', function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'project-card.tpl.html'
+	};
+});
+							
